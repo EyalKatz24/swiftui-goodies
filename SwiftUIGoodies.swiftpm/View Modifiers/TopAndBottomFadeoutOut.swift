7796175968db283ea98479001e63d63a76ef37bb
Eyal Katz <eyalkatz2403@gmail.com>
2025-03-05 00:40:40 +0200

    Create TopAndBottomFadeoutOut.swift

---
+++ SwiftUIGoodies.swiftpm/View Modifiers/TopAndBottomFadeoutOut.swift
@@ -0,0 +1,43 @@
+//
+//  TopAndBottomFadeoutOut.swift
+//  SwiftUIGoodies
+//
+//  Created by Eyal Katz on 05/03/2025.
+//
+
+import SwiftUI
+
+fileprivate struct TopAndBottomFadeOut: ViewModifier {
+    var fadingColor: Color
+    var gradientHeight: CGFloat
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            
+            VStack {
+                LinearGradient(
+                    colors: [fadingColor, fadingColor.opacity(0)/* SwiftUI bug */],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .frame(height: gradientHeight)
+                
+                Spacer()
+                
+                LinearGradient(
+                    colors: [fadingColor.opacity(0)/* SwiftUI bug */, fadingColor],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .frame(height: gradientHeight)
+            }
+        }
+    }
+}
+
+extension View {
+    func topAndBottomFadeOut(fadingColor: Color = .init(uiColor: .systemBackground), gradientHeight: CGFloat) -> some View {
+        modifier(TopAndBottomFadeOut(fadingColor: fadingColor, gradientHeight: gradientHeight))
+    }
+}
